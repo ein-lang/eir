a23d7fd6b4f82ba59927451969a603e1653a91d0
Yota Toyama <raviqqe@gmail.com>
2021-04-24 06:26:18 +0000

    Refactor unfolding

--- ssf/src/types/unfold/mod.rs
+++ ssf/src/types/unfold/mod.rs
@@ -1,17 +1,41 @@
-mod type_unfolder;
-
 use crate::types::*;
-use type_unfolder::TypeUnfolder;
 
 pub(crate) fn unfold(record: &Record) -> Record {
     Record::new(
         record
             .elements()
             .iter()
-            .map(|type_| canonicalize(&TypeUnfolder::new(record).unfold(type_)))
+            .map(|type_| canonicalize(&unfold_with_root(type_, record, 0)))
             .collect(),
         record.is_boxed(),
     )
+}
+
+fn unfold_with_root(type_: &Type, root_record: &Record, root_index: usize) -> Type {
+    let unfold = |type_| unfold_with_root(type_, root_record, root_index);
+
+    match type_ {
+        Type::Function(function) => {
+            Function::new(unfold(function.argument()), unfold(function.result())).into()
+        }
+        Type::Index(index) => {
+            if *index == root_index {
+                root_record.clone().into()
+            } else {
+                Type::Index(*index)
+            }
+        }
+        Type::Record(record) => Record::new(
+            record
+                .elements()
+                .iter()
+                .map(|type_| unfold_with_root(type_, root_record, root_index + 1))
+                .collect(),
+            record.is_boxed(),
+        )
+        .into(),
+        Type::Primitive(_) | Type::Variant => type_.clone(),
+    }
 }
 
 #[cfg(test)]
@@ -21,7 +45,7 @@ mod tests {
 
     #[test]
     fn unfold_() {
-        for (type_, unfolded_type) in &[
+        for (type_, root_type) in &[
             (
                 Algebraic::new(vec![Constructor::boxed(vec![Type::Index(0)])]),
                 Algebraic::new(vec![Constructor::boxed(vec![Algebraic::new(vec![
@@ -61,7 +85,7 @@ mod tests {
                 .into()])]),
             ),
         ] {
-            assert_eq!(&unfold(type_), unfolded_type);
+            assert_eq!(&unfold(type_), root_type);
         }
     }
 }
