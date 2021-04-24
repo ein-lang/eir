95189c73a52ed3b856376b26de930a363dce4b2d
Yota Toyama <raviqqe@gmail.com>
2021-04-24 16:41:53 +0000

    Fix record equality tests

--- ssf/src/types/canonicalize/record_equality.rs
+++ ssf/src/types/canonicalize/record_equality.rs
@@ -61,75 +61,61 @@ mod tests {
                 Function::new(Primitive::Float64, Primitive::Float64).into(),
             ),
             (
-                Record::new(vec![Constructor::boxed(vec![Primitive::Float64.into()])]).into(),
-                Record::new(vec![Constructor::boxed(vec![Primitive::Float64.into()])]).into(),
+                Record::new(vec![Primitive::Float64.into()], true).into(),
+                Record::new(vec![Primitive::Float64.into()], true).into(),
             ),
             (
-                Record::new(vec![Constructor::boxed(vec![Type::Index(0)])]).into(),
-                Record::new(vec![Constructor::boxed(vec![Record::new(vec![
-                    Constructor::boxed(vec![Type::Index(0)]),
-                ])
-                .into()])])
-                .into(),
+                Record::new(vec![Type::Index(0)], true).into(),
+                Record::new(vec![Record::new(vec![Type::Index(0)], true).into()], true).into(),
             ),
             (
-                Record::new(vec![Constructor::boxed(vec![Type::Index(0)])]).into(),
-                Record::new(vec![Constructor::boxed(vec![Record::new(vec![
-                    Constructor::boxed(vec![Type::Index(1)]),
-                ])
-                .into()])])
-                .into(),
+                Record::new(vec![Type::Index(0)], true).into(),
+                Record::new(vec![Record::new(vec![Type::Index(1)], true).into()], true).into(),
             ),
             (
-                Record::new(vec![Constructor::boxed(vec![Record::new(vec![
-                    Constructor::boxed(vec![Type::Index(0)]),
-                ])
-                .into()])])
-                .into(),
-                Record::new(vec![Constructor::boxed(vec![Record::new(vec![
-                    Constructor::boxed(vec![Type::Index(1)]),
-                ])
-                .into()])])
-                .into(),
+                Record::new(vec![Record::new(vec![Type::Index(0)], true).into()], true).into(),
+                Record::new(vec![Record::new(vec![Type::Index(1)], true).into()], true).into(),
             ),
             (
-                Record::new(vec![Constructor::boxed(vec![Function::new(
-                    Primitive::Float64,
-                    Type::Index(0),
+                Record::new(
+                    vec![Function::new(Primitive::Float64, Type::Index(0)).into()],
+                    true,
                 )
-                .into()])])
                 .into(),
-                Record::new(vec![Constructor::boxed(vec![Function::new(
-                    Primitive::Float64,
-                    Record::new(vec![Constructor::boxed(vec![Function::new(
+                Record::new(
+                    vec![Function::new(
                         Primitive::Float64,
-                        Type::Index(0),
+                        Record::new(
+                            vec![Function::new(Primitive::Float64, Type::Index(0)).into()],
+                            true,
+                        ),
                     )
-                    .into()])]),
+                    .into()],
+                    true,
                 )
-                .into()])])
                 .into(),
             ),
             (
-                Record::new(vec![Constructor::boxed(vec![Function::new(
-                    Primitive::Float64,
-                    Type::Index(0),
+                Record::new(
+                    vec![Function::new(Primitive::Float64, Type::Index(0)).into()],
+                    true,
                 )
-                .into()])])
                 .into(),
-                Record::new(vec![Constructor::boxed(vec![Function::new(
-                    Primitive::Float64,
-                    Record::new(vec![Constructor::boxed(vec![Function::new(
+                Record::new(
+                    vec![Function::new(
                         Primitive::Float64,
-                        Type::Index(1),
+                        Record::new(
+                            vec![Function::new(Primitive::Float64, Type::Index(1)).into()],
+                            true,
+                        ),
                     )
-                    .into()])]),
+                    .into()],
+                    true,
                 )
-                .into()])])
                 .into(),
             ),
         ] {
-            assert!(TypeEqualityChecker::new(&[]).equal(one, other));
+            assert!(equal_with_pairs(one, other, &[]));
         }
     }
 
@@ -149,19 +135,19 @@ mod tests {
                 Function::new(Primitive::Float64, Primitive::Float64).into(),
             ),
             (
-                Record::new(vec![Constructor::boxed(vec![Primitive::Float64.into()])]).into(),
-                Record::new(vec![Constructor::boxed(vec![
-                    Primitive::Float64.into(),
-                    Primitive::Float64.into(),
-                ])])
+                Record::new(vec![Primitive::Float64.into()], true).into(),
+                Record::new(
+                    vec![Primitive::Float64.into(), Primitive::Float64.into()],
+                    true,
+                )
                 .into(),
             ),
             (
-                Record::new(vec![Constructor::boxed(vec![Primitive::Float64.into()])]).into(),
-                Record::new(vec![Constructor::unboxed(vec![Primitive::Float64.into()])]).into(),
+                Record::new(vec![Primitive::Float64.into()], true).into(),
+                Record::new(vec![Primitive::Float64.into()], false).into(),
             ),
         ] {
-            assert!(!TypeEqualityChecker::new(&[]).equal(one, other));
+            assert!(!equal_with_pairs(one, other, &[]));
         }
     }
 }
