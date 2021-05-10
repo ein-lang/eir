487fe53a11893bef1a25d613702abc2dd9dfacbd
Yota Toyama <raviqqe@gmail.com>
2021-05-10 02:00:03 +0000

    Convert expressions

--- eir/src/analysis/reference_count/mod.rs
+++ eir/src/analysis/reference_count/mod.rs
@@ -15,5 +15,31 @@ pub fn count_references(module: &Module) -> Module {
 }
 
 fn convert_definition(definition: &Definition) -> Definition {
-    todo!()
+    Definition::with_options(
+        definition.name(),
+        definition.environment().to_vec(),
+        definition.arguments().to_vec(),
+        convert_expression(definition.body()),
+        definition.result_type().clone(),
+        definition.is_thunk(),
+    )
+}
+
+fn convert_expression(expression: &Expression) -> Expression {
+    match expression {
+        Expression::ArithmeticOperation(_) => todo!(),
+        Expression::Boolean(_) => todo!(),
+        Expression::ByteString(_) => todo!(),
+        Expression::Case(_) => todo!(),
+        Expression::ComparisonOperation(_) => todo!(),
+        Expression::FunctionApplication(_) => todo!(),
+        Expression::If(_) => todo!(),
+        Expression::Let(_) => todo!(),
+        Expression::LetRecursive(_) => todo!(),
+        Expression::Number(_) => todo!(),
+        Expression::Record(_) => todo!(),
+        Expression::RecordElement(_) => todo!(),
+        Expression::Variable(_) => todo!(),
+        Expression::Variant(_) => todo!(),
+    }
 }
