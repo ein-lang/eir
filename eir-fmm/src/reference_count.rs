f6b9e95fb0324650e84dbfe5ab91b3c15c4cba53
Yota Toyama <raviqqe@gmail.com>
2021-05-15 03:52:51 +0000

    Compile string variable

--- eir-fmm/src/reference_count.rs
+++ eir-fmm/src/reference_count.rs
@@ -1,10 +1,16 @@
+use crate::types;
+
 pub fn clone_variable(
     instruction_builder: &fmm::build::InstructionBuilder,
     variable: &str,
     type_: &eir::types::Type,
 ) -> Result<(), fmm::build::BuildError> {
     match type_ {
-        eir::types::Type::ByteString => todo!(),
+        eir::types::Type::ByteString => {
+            let _ = fmm::build::variable(variable, types::compile_string());
+
+            todo!();
+        }
         eir::types::Type::Function(_) => todo!(),
         eir::types::Type::Record(_) => todo!(),
         eir::types::Type::Variant => todo!(),
