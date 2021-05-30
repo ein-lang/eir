c7b221aeb92916aa1c541a8dce1a7cf823d970bf
Yota Toyama <raviqqe@gmail.com>
2021-05-30 19:56:32 +0000

    Fix reference counting of boxed payloads in variants

--- eir-fmm/src/reference_count/mod.rs
+++ eir-fmm/src/reference_count/mod.rs
@@ -9,7 +9,7 @@ mod variants;
 pub use expressions::*;
 pub use functions::*;
 pub use heap::*;
-pub use pointers::{compile_tagged_pointer, compile_untagged_pointer};
+pub use pointers::{compile_tagged_pointer, compile_untagged_pointer, drop_pointer};
 pub use records::*;
 pub use variants::*;
 
