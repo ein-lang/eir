be83ff1bbca870807673856aaa21e0afbca775e0
Yota Toyama <raviqqe@gmail.com>
2021-06-02 01:43:14 +0000

    Fix free instruction use

--- eir-fmm/src/reference_count/heap.rs
+++ eir-fmm/src/reference_count/heap.rs
@@ -7,10 +7,17 @@ pub fn allocate_heap(
     builder: &fmm::build::InstructionBuilder,
     type_: impl Into<fmm::types::Type>,
 ) -> Result<fmm::build::TypedExpression, CompileError> {
-    let pointer = builder.allocate_heap(fmm::build::size_of(fmm::types::Record::new(vec![
-        COUNT_TYPE.into(),
-        type_.into(),
-    ])));
+    let type_ = type_.into();
+    let pointer = fmm::build::bit_cast(
+        fmm::types::Pointer::new(fmm::types::Record::new(vec![
+            COUNT_TYPE.into(),
+            type_.clone(),
+        ])),
+        builder.allocate_heap(fmm::build::size_of(fmm::types::Record::new(vec![
+            COUNT_TYPE.into(),
+            type_,
+        ]))),
+    );
 
     builder.store(
         fmm::ir::Primitive::PointerInteger(INITIAL_COUNT as i64),
@@ -24,13 +31,16 @@ pub fn free_heap(
     builder: &fmm::build::InstructionBuilder,
     pointer: impl Into<fmm::build::TypedExpression>,
 ) -> Result<(), CompileError> {
-    builder.free_heap(builder.pointer_address(
-        fmm::build::bit_cast(
-            fmm::types::Pointer::new(fmm::types::Primitive::PointerInteger),
-            pointer,
-        ),
-        fmm::ir::Primitive::PointerInteger(-1),
-    )?);
+    builder.free_heap(fmm::build::bit_cast(
+        fmm::types::GENERIC_POINTER_TYPE.clone(),
+        builder.pointer_address(
+            fmm::build::bit_cast(
+                fmm::types::Pointer::new(fmm::types::Primitive::PointerInteger),
+                pointer,
+            ),
+            fmm::ir::Primitive::PointerInteger(-1),
+        )?,
+    ));
 
     Ok(())
 }
